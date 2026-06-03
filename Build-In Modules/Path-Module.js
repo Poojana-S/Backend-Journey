91495a94c4b9a760092d6965f107e66c2159c918
08021976-22022006 <poojanas.dev@gmail.com>
2026-06-03 07:13:21 +0530

    Path Module

---
+++ Build-In Modules/Path-Module.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+// Separator of the path
+console.log(path.sep)
+
+// Join method -> To join the sequence of path segments using the platform specific separator as a delimiter
+// then normalizes the resulting path.
+// Trailing slash is removed automatically
+// eg. path.join('/content/', 'subfolder', 'test.txt') --> /content/subfolder/test.txt will be the output
+const filepath = path.join('Modules', 'Names.js') // any path can be form be form
+console.log(filepath)
+
+// To get the ast portion of the path
+const basename = path.basename(filepath)
+console.log(basename)
+
+// To get the absolute path, Absolute path is the full path from the root to the file
+// Some systems have different root directories, so this can be useful to get the absolute path of a file
+const absolute = path.resolve(__dirname, 'Build-In Modules', 'OS-Module.js')
+console.log(absolute)
